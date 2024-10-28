72992cdb472ff80cd7048cecb8cc4de22d4dda71
brightalot <gamja9151@hanyang.ac.kr>
2024-10-28 22:18:27 +0900

    feat : reviews 모델 추가

--- meomeoknyang/urls.py
+++ meomeoknyang/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('restaurants.urls')),  # restaurants 앱
+    path('', include('reviews.urls')),  # restaurants 앱
 ]
