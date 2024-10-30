a0107fe198bbfa3175027f0e9d865ec195df3fba
brightalot <gamja9151@hanyang.ac.kr>
2024-10-30 10:30:37 +0900

    feat : 마이페이지 기능 추가
    닉네임 수정 기능
    user 프로필 조회 기능

--- users/models.py
+++ users/models.py
@@ -1,7 +1,11 @@
 # users/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from restaurants.models import Restaurant
+# from restaurants.models import Restaurant
+# from cafe.models import Cafe
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from baseplace.models import BasePlace
 
 class CustomUser(AbstractUser):
     TITLE_CHOICES = [
@@ -16,10 +20,21 @@ class CustomUser(AbstractUser):
     name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     department = models.CharField(max_length=100, blank=True, null=True)
-    recent_stamp_restaurants = models.ManyToManyField(Restaurant, blank=True, related_name='stamped_by')
+    # recent_stamp_restaurants = models.ManyToManyField(Restaurant, blank=True, related_name='stamped_by')
+    # recent_stamp_cafes = models.ManyToManyField(Cafe, blank=True, related_name='stamped_by')
+
+    # Generic relation
+    recent_stamp_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+    recent_stamp_object_id = models.PositiveIntegerField(null=True)
+    recent_stamp_places = GenericForeignKey('recent_stamp_content_type', 'recent_stamp_object_id')
+
 
     def __str__(self):
         return self.nickname
 
+    # def get_recent_stamps(self):
+    #     return self.recent_stamp_restaurants.order_by('-id')[:10]  # 최근 방문한 최대 10개 식당 반환
+
     def get_recent_stamps(self):
-        return self.recent_stamp_restaurants.order_by('-id')[:10]  # 최근 방문한 최대 10개 식당 반환
+        # 최근 방문한 장소
+        return self.recent_stamp_places.all().order_by('-id')[:10]
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from .serializers import UserRegistrationSerializer
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class UserRegistrationView(APIView):
     def post(self, request):
@@ -17,3 +18,27 @@ class UserDetailView(generics.RetrieveAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     lookup_field = 'id'  # URL에서 사용자의 ID를 기준으로 조회
+
+# 현재 로그인된 사용자 정보 반환
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]  # 로그인된 사용자만 접근 가능
+
+    def get(self, request):
+        user = request.user
+        serializer = CustomUserSerializer(user)
+        return Response(serializer.data)
+    
+# 닉네임 업데이트
+class UserUpdateView(APIView):
+    permission_classes = [IsAuthenticated]  # 로그인된 사용자만 접근 가능
+
+    def patch(self, request):
+        user = request.user
+        data = request.data
+
+        # 닉네임 업데이트
+        if 'nickname' in data:
+            user.nickname = data['nickname']
+
+        user.save()
+        return Response({"message": "프로필 정보가 업데이트되었습니다."}, status=status.HTTP_200_OK)
