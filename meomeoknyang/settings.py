74057a6e2c65005232cb9d2fbbd2366be0f5fbce
brightalot <gamja9151@hanyang.ac.kr>
2024-11-13 15:17:23 +0900

    db 연결

--- meomeoknyang/settings.py
+++ meomeoknyang/settings.py
@@ -99,10 +99,27 @@ WSGI_APPLICATION = "meomeoknyang.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+import os
+from dotenv import load_dotenv
+
+# .env 파일 로드
+load_dotenv()
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
